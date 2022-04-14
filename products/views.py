7bd263e3d6be8ee554646213d0b1d6338f6237b7
elijahrosett <rosett3@gmail.com>
2022-04-14 10:14:24 -0600

    Created post method in views + registered products model with admin site

--- products/views.py
+++ products/views.py
@@ -1,8 +1,28 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Products
+from .serializers import ProductsSerializer
+from products import serializers
 
-@api_view(['GET'])
+
+
+
+
+@api_view(['GET', 'POST'])
 def products_list(request):
-    return Response('ok')
+
+    if request.method == 'GET':
+        products = Products.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+    elif request.method == 'POST':
+        serializer = ProductsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True) 
+        serializer.save()   
+        return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
+
 
 
--- products_project/settings.py
+++ products_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,6 +71,9 @@ TEMPLATES = [
     },
 ]
 
+
+
+
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
@@ -116,3 +120,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': FALSE
+}
