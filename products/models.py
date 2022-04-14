4ffbcb9a2080022e60cc14b8184c3409690833ac
elijahrosett <rosett3@gmail.com>
2022-04-14 08:58:24 -0600

    Created products model for product app + registered products and rest framework in project settings

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
+from unicodedata import decimal
 from django.db import models
+from django.forms import CharField, DecimalField
 
 # Create your models here.
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory_quantity = models.IntegerField()
